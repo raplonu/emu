72247112e97df41b05fb16e49ab6559ed78e0bc2
raplonu <raplonu.jb@gmail.com>
2021-07-29 16:47:35 +1000

    cuda_arch: Adds error handling.

--- tools/conanfile.py
+++ tools/conanfile.py
@@ -1,6 +1,13 @@
 from conans import ConanFile
 
-from cuda_arch import compute_capabilities
+try:
+    from cuda_arch import compute_capabilities
+except :
+    print('Error importing cuda_arch: cannot load cuda library.')
+
+# def compute_capabilities():
+#     import cuda_arch
+#     return cuda_arch.compute_capabilities()
 
 class Pkg(ConanFile):
     exports = 'cuda_arch.py'
