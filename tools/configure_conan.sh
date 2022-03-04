53dcd1d5b9cf128a7e3d3c82f29b5de94f0fbca6
raplonu <raplonu.jb@gmail.com>
2022-03-04 19:55:41 +1100

    CICD: Install tox.
    CICD: Use python 3.8.
    
    CICD: Update remote for real.
    
    CICD: Gnnnn.
    
    CICD: Gnnnn.
    
    CICD: Gnnnn.
    
    CICD: Gnnnn.
    
    CICD: Gnnnn.
    
    CICD: Gnnnn.
    
    CICD: Gnnnn.
    
    CICD: Gnnnn.
    
    CICD: Gnnnn.
    
    CICD: Gnnnn.
    
    CICD: Gnnnn.
    
    CICD: Gnnnn.
    
    CICD: Gnnnn.
    
    CICD: Gnnnn.
    
    CICD: Gnnnn.
    
    CICD: Docs.
    
    CICD: Docs.
    
    CICD: Docs.
    
    CICD: Docs.
    
    CICD: Docs.
    
    CICD: Docs.

--- tools/configure_conan.sh
+++ tools/configure_conan.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
 LOCAL_DIR="$(realpath --relative-to=$(pwd) $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd ))"
 
-if ! command -v conan &> /dev/null
-then
-    echo "conan could not be found"
-    while true; do
-        read -p "Do you wish to install this program? [Yy/Nn]" yn
-        case $yn in
-            [Yy]* ) pip install conan --upgrade; break;;
-            [Nn]* ) exit;;
-            * ) echo "Please answer yes or no.";;
-        esac
-    done
-fi
+# if ! command -v conan &> /dev/null
+# then
+#     echo "conan could not be found"
+#     while true; do
+#         read -p "Do you wish to install this program? [Yy/Nn]" yn
+#         case $yn in
+#             [Yy]* ) pip install conan --upgrade; break;;
+#             [Nn]* ) exit;;
+#             * ) echo "Please answer yes or no.";;
+#         esac
+#     done
+# fi
 
-# Adds cosmic conan repository if it is not already the case.
 conan remote list | grep cosmic || conan remote add cosmic https://odp2.jfrog.io/artifactory/api/conan/cosmic
+
+conan profile list | grep default || conan profile new default --detect
+
+conan profile update settings.compiler.libcxx=libstdc++11 default
+
+echo "Current state"
+
+conan remote list
+conan profile show default
